ebc6f016e3aa8d429027bf1b4fc309fdaffa6669
bipartner <bipartner@viacom18.com>
2018-05-15 14:35:04 +0000

    Deleted CMS.sh

--- target/CMS.sh
+++
@@ -1,2 +0,0 @@
-
-spark-submit --class com.viacom18.CMS.MonthlyLoading --master yarn --deploy-mode cluster  --executor-memory 30g --executor-cores 8 --num-executors 14  /home/biappadmin/CMS/Project_Pi-1.0-SNAPSHOT.jar 
